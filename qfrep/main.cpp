43fdb108a5235aa6c66b8c276509113b75aba08a
Quentin Corker_Marin <quentin@Quentins-MacBook-Pro.local>
2015-05-31 09:24:24 +0100

    Initial Commit

---
+++ qfrep/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  qfrep
+//
+//  Created by Quentin Corker_Marin on 31/05/2015.
+//  Copyright (c) 2015 Quentin Corker-Marin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
